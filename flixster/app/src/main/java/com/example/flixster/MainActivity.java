9d281d41a532c6a92526feb4a002ed323b56bb6a
Seaney Shell <sfs185@humboldt.edu>
2019-12-22 14:35:37 -0800

    Commit

--- flixster/app/src/main/java/com/example/flixster/MainActivity.java
+++ flixster/app/src/main/java/com/example/flixster/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    AsyncHttpClient client = new AsyncHttpClient();
+    RequestParams params = new RequestParams();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_main);
     }
 }
